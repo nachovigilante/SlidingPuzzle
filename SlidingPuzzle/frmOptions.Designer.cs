d4986645f0c908d395c886a30bbbb6c4d1fd766b
nachovigilante <nachovigilante@gmail.com>
2018-06-16 06:28:21 -0300

    Arregle algo estetico

--- SlidingPuzzle/frmOptions.Designer.cs
+++ SlidingPuzzle/frmOptions.Designer.cs
@@ -194,11 +194,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(90, 486);
+            this.button1.Location = new System.Drawing.Point(408, 355);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(95, 25);
             this.button1.TabIndex = 9;
-            this.button1.Text = "button1";
+            this.button1.Text = "Probar en juego";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
