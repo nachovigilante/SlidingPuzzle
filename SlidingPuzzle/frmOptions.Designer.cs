60ab6cf03377deaa5307186dc10368adccc75e70
nachovigilante <nachovigilante@gmail.com>
2018-06-16 06:06:42 -0300

    Opciones y principio de division de imagenes

--- SlidingPuzzle/frmOptions.Designer.cs
+++ SlidingPuzzle/frmOptions.Designer.cs
@@ -28,19 +28,209 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.chkAnimations = new System.Windows.Forms.CheckBox();
+            this.lblAnimations = new System.Windows.Forms.Label();
+            this.grpAnimations = new System.Windows.Forms.GroupBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.lblFlu = new System.Windows.Forms.Label();
+            this.trkAnimations = new System.Windows.Forms.TrackBar();
+            this.lblVel = new System.Windows.Forms.Label();
+            this.lblSize = new System.Windows.Forms.Label();
+            this.cboSize = new System.Windows.Forms.ComboBox();
+            this.toolTipAnimations = new System.Windows.Forms.ToolTip(this.components);
+            this.chkKeys = new System.Windows.Forms.CheckBox();
+            this.lblKeys = new System.Windows.Forms.Label();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.grpAnimations.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trkAnimations)).BeginInit();
             this.SuspendLayout();
+            // 
+            // chkAnimations
+            // 
+            this.chkAnimations.AutoSize = true;
+            this.chkAnimations.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.chkAnimations.Checked = true;
+            this.chkAnimations.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkAnimations.Location = new System.Drawing.Point(170, 86);
+            this.chkAnimations.Name = "chkAnimations";
+            this.chkAnimations.Size = new System.Drawing.Size(15, 14);
+            this.chkAnimations.TabIndex = 0;
+            this.toolTipAnimations.SetToolTip(this.chkAnimations, "las animaciones pueden aumentar el tiempo y pueden ser molestas, no se recomienda" +
+        "n si quiere resolverlo con un mejor tiempo");
+            this.chkAnimations.UseVisualStyleBackColor = true;
+            this.chkAnimations.CheckedChanged += new System.EventHandler(this.chkAnimations_CheckedChanged);
+            // 
+            // lblAnimations
+            // 
+            this.lblAnimations.AutoSize = true;
+            this.lblAnimations.Location = new System.Drawing.Point(59, 86);
+            this.lblAnimations.Name = "lblAnimations";
+            this.lblAnimations.Size = new System.Drawing.Size(107, 13);
+            this.lblAnimations.TabIndex = 1;
+            this.lblAnimations.Text = "Animaciones activas:";
+            // 
+            // grpAnimations
+            // 
+            this.grpAnimations.Controls.Add(this.trackBar1);
+            this.grpAnimations.Controls.Add(this.lblFlu);
+            this.grpAnimations.Controls.Add(this.trkAnimations);
+            this.grpAnimations.Controls.Add(this.lblVel);
+            this.grpAnimations.Location = new System.Drawing.Point(52, 122);
+            this.grpAnimations.Name = "grpAnimations";
+            this.grpAnimations.Size = new System.Drawing.Size(200, 182);
+            this.grpAnimations.TabIndex = 3;
+            this.grpAnimations.TabStop = false;
+            this.grpAnimations.Text = "Configuración de las animaciones";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.Location = new System.Drawing.Point(13, 127);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(174, 35);
+            this.trackBar1.TabIndex = 3;
+            // 
+            // lblFlu
+            // 
+            this.lblFlu.AutoSize = true;
+            this.lblFlu.Location = new System.Drawing.Point(10, 105);
+            this.lblFlu.Name = "lblFlu";
+            this.lblFlu.Size = new System.Drawing.Size(43, 13);
+            this.lblFlu.TabIndex = 2;
+            this.lblFlu.Text = "Fluidez:";
+            // 
+            // trkAnimations
+            // 
+            this.trkAnimations.AutoSize = false;
+            this.trkAnimations.Location = new System.Drawing.Point(13, 59);
+            this.trkAnimations.Name = "trkAnimations";
+            this.trkAnimations.Size = new System.Drawing.Size(174, 35);
+            this.trkAnimations.TabIndex = 1;
+            // 
+            // lblVel
+            // 
+            this.lblVel.AutoSize = true;
+            this.lblVel.Location = new System.Drawing.Point(10, 37);
+            this.lblVel.Name = "lblVel";
+            this.lblVel.Size = new System.Drawing.Size(57, 13);
+            this.lblVel.TabIndex = 0;
+            this.lblVel.Text = "Velocidad:";
+            // 
+            // lblSize
+            // 
+            this.lblSize.AutoSize = true;
+            this.lblSize.Location = new System.Drawing.Point(59, 50);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(101, 13);
+            this.lblSize.TabIndex = 4;
+            this.lblSize.Text = "Tamaño del tablero:";
+            // 
+            // cboSize
+            // 
+            this.cboSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboSize.FormattingEnabled = true;
+            this.cboSize.Items.AddRange(new object[] {
+            "3x3",
+            "4x4",
+            "5x5"});
+            this.cboSize.Location = new System.Drawing.Point(166, 46);
+            this.cboSize.Name = "cboSize";
+            this.cboSize.Size = new System.Drawing.Size(66, 21);
+            this.cboSize.TabIndex = 5;
+            // 
+            // toolTipAnimations
+            // 
+            this.toolTipAnimations.AutomaticDelay = 200;
+            this.toolTipAnimations.AutoPopDelay = 5000;
+            this.toolTipAnimations.InitialDelay = 200;
+            this.toolTipAnimations.ReshowDelay = 40;
+            this.toolTipAnimations.ToolTipTitle = "Animaciones";
+            // 
+            // chkKeys
+            // 
+            this.chkKeys.AutoSize = true;
+            this.chkKeys.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.chkKeys.Checked = true;
+            this.chkKeys.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkKeys.Location = new System.Drawing.Point(170, 323);
+            this.chkKeys.Name = "chkKeys";
+            this.chkKeys.Size = new System.Drawing.Size(15, 14);
+            this.chkKeys.TabIndex = 7;
+            this.toolTipAnimations.SetToolTip(this.chkKeys, "las animaciones pueden aumentar el tiempo y pueden ser molestas, no se recomienda" +
+        "n si quiere resolverlo con un mejor tiempo");
+            this.chkKeys.UseVisualStyleBackColor = true;
+            // 
+            // lblKeys
+            // 
+            this.lblKeys.AutoSize = true;
+            this.lblKeys.Location = new System.Drawing.Point(59, 323);
+            this.lblKeys.Name = "lblKeys";
+            this.lblKeys.Size = new System.Drawing.Size(97, 13);
+            this.lblKeys.TabIndex = 6;
+            this.lblKeys.Text = "Uso con las teclas:";
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.Location = new System.Drawing.Point(59, 360);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(173, 13);
+            this.lblFile.TabIndex = 8;
+            this.lblFile.Text = "Agregar una imágen personalizada:";
+            // 
+            // btnFile
+            // 
+            this.btnFile.Location = new System.Drawing.Point(238, 353);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(134, 27);
+            this.btnFile.TabIndex = 0;
+            this.btnFile.Text = "Seleccionar imagen";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
             // 
             // frmOptions
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(524, 312);
+            this.ClientSize = new System.Drawing.Size(637, 738);
+            this.Controls.Add(this.btnFile);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.chkKeys);
+            this.Controls.Add(this.lblKeys);
+            this.Controls.Add(this.cboSize);
+            this.Controls.Add(this.lblSize);
+            this.Controls.Add(this.grpAnimations);
+            this.Controls.Add(this.lblAnimations);
+            this.Controls.Add(this.chkAnimations);
             this.Name = "frmOptions";
-            this.Text = "frmOptions";
+            this.Text = "Opciones";
+            this.grpAnimations.ResumeLayout(false);
+            this.grpAnimations.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trkAnimations)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox chkAnimations;
+        private System.Windows.Forms.Label lblAnimations;
+        private System.Windows.Forms.GroupBox grpAnimations;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label lblFlu;
+        private System.Windows.Forms.TrackBar trkAnimations;
+        private System.Windows.Forms.Label lblVel;
+        private System.Windows.Forms.Label lblSize;
+        private System.Windows.Forms.ComboBox cboSize;
+        private System.Windows.Forms.ToolTip toolTipAnimations;
+        private System.Windows.Forms.Label lblKeys;
+        private System.Windows.Forms.CheckBox chkKeys;
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.Button btnFile;
     }
 }
--- SlidingPuzzle/frmOptions.cs
+++ SlidingPuzzle/frmOptions.cs
@@ -16,5 +16,49 @@ namespace SlidingPuzzle
         {
             InitializeComponent();
         }
+
+        private void chkAnimations_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Abrir una imagen";
+                dlg.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png)|*.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    Bitmap userImage = new Bitmap(dlg.FileName);
+                    Bitmap resizedImage = new Bitmap(userImage, new Size(200, 200));
+                    int size = 3;
+                    PictureBox[,] pbArray = new PictureBox[size, size];
+                    int tileSize = (int)Math.Floor((float)resizedImage.Width / size);
+                    for (int x = 0; x < size; x++)
+                    {
+                        for (int y = 0; y < size; y++)
+                        {
+                            Rectangle cloneRect = new Rectangle(tileSize * x, tileSize * y, tileSize, tileSize);
+                            System.Drawing.Imaging.PixelFormat format = resizedImage.PixelFormat;
+                            Bitmap tile = resizedImage.Clone(cloneRect, format);
+
+                            pbArray[x, y] = new PictureBox();
+                            Controls.Add(pbArray[x, y]);
+                            pbArray[x, y].Name = "pbArray_" + x + "_" + y;
+                            pbArray[x, y].Size = new Size(tileSize, tileSize);
+                            pbArray[x, y].BackColor = Color.White;
+                            pbArray[x, y].BackgroundImageLayout = ImageLayout.Stretch;
+                            pbArray[x, y].Location = new Point(lblFile.Location.X + tileSize * x + 5 * x, lblFile.Location.Y + 5 * y + tileSize * y + 50);
+                            pbArray[x, y].Anchor = AnchorStyles.Left;
+                            pbArray[x, y].Visible = true;
+                            pbArray[x, y].Image = tile;
+                        }
+                    }
+
+                }
+            }
+        }
     }
 }
